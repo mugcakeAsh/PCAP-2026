09bcc4849b7c45b52c5718f766e9439d195e6b65
ash <20261ctb0100039@estudantes.ifpr.edu.br>
2026-04-07 17:20:18 +0000

    Aula 10 - 1000: Hello World

---
+++ beecrowd/1000.py
@@ -0,0 +1,13 @@
+'''
+Problema Beecrowd | 1000
+Data: 2026.02.01
+Estudante: Gabriela
+'''
+# Objetivo: fazer com que a mensagem "Hello World!" apareça na tela
+
+# --- ANÁLISE (LIAC) ---
+# Entrada: Este problema não possui nenhuma entrada
+# Processamento:
+# Saída: aparecer a mensagem "Hello World!"
+
+print("Hello World!")
